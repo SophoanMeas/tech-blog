0d3d6e49ba16367c6d2d730c69cc4680a07a063f
Sophoan Meas <sophoan.meas@hotmail.com>
2022-07-18 23:54:24 -0400

    add user models

---
+++ models/User.js
@@ -0,0 +1,57 @@
+const { Model, DataTypes } = require('sequelize');
+const sequlize = require('../config/connection');
+const bcrypt = require('bcrypt');
+
+class User extends Model {
+  checkPassword(loginPwd) {
+    return bcrypt.compareSync(loginPwd, this.password);
+  }
+}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      valide: {
+        len: [6],
+      },
+    },
+  },
+  {
+    hooks: {
+      async beforeCreate(newUserData) {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+    },
+    sequelize,
+    freezeTableName: true,
+    timestamps: false,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;
