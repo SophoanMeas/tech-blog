888eb085103d3d944d5fc784c8c681fc7daf0238
Sophoan Meas <sophoan.meas@hotmail.com>
2022-07-19 22:20:32 -0400

    add comment router

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -4,15 +4,49 @@ const withAuth = require('../../utils/auth');
 const { Comment } = require('../../models');
 
 // get all comments
-router.get('/', async (req, res) =>{
-    try {
-        
-        const commentData = await Comment.findAll({
-            
-        })
+router.get('/', async (req, res) => {
+  try {
+    const commentData = await Comment.findAll({});
+    res.status(200).json(commentData);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 
-    } catch (error) {
-        console.log(err);
-		res.status(500).json(err);
+// create new comment
+router.get('/', withAuth, async (req, res) => {
+  if (req.session) {
+    try {
+      const commentData = await Comment.create({
+        comment: req.body.comment,
+        postId: req.body.postId,
+        userId: req.session.userId,
+      });
+      res.status(200).json(commentData);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json(err);
     }
-})
+  }
+});
+
+// delete comment
+router.get('/:id', withAuth, async (req, res) => {
+  try {
+    const commentData = await Comment.destroy({
+      where: {
+        id: req.params.ud,
+      },
+    });
+    if (!commentData) {
+      res.status(400).json({ response: 'No comment found!' });
+      return;
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
